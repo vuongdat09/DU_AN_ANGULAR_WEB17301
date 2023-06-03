57089e119bf656f55b0da147ecfb80dd51edbe18
vuongdat09 <110186825+vuongdat09@users.noreply.github.com>
2023-06-03 21:17:06 +0700

    second commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/angularDB");
+
+export default viteNodeApp = app
