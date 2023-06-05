22841a1e42aba3c8adf8f3de324b02206577043e
IceTea <iceteawebdev@gmail.com>
2023-06-05 22:40:26 +0700

    test

---
+++ src/models/Test.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const categoryShema = new mongoose.Schema(
+  {
+    name: String,
+    desc: String,
+    img: String,
+    products: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export default mongoose.model("Category", categoryShema);
